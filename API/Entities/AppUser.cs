1560472cbc7b45b2cd2f692295c98c46f3593fb4
Nic Findlay <nicisme@gmail.com>
2023-02-20 08:38:44 +0200

    Primary key note

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,8 +1,11 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Entities
 {
     public class AppUser
     {
+        //use [Key] if primarykey not Id
         public int Id { get; set; }
         public string UserName { get; set; } 
     }
