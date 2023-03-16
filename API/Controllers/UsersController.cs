5da01114c50e549aa0f5e79ba42417d23b545227
Nic Findlay <nicisme@gmail.com>
2023-03-16 13:56:39 +0200

    API Controller - endpoint to Users

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+
+using Microsoft.AspNetCore.Mvc;
+using API.Data;
+using API.Entities;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+        _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers(){
+            var users = _context.Users.ToList();
+
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id){
+            return _context.Users.Find(id);
+        }
+
+    }
+}
